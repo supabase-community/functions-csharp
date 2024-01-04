081e1ebddbb30a5f044eb5e4d0f5cd1bac569397
FantasyTeddy <matthiasbaer@bluewin.ch>
2024-01-04 14:40:02 +0100

    Enable local test execution with docker-compose

--- FunctionsTests/ClientTests.cs
+++ FunctionsTests/ClientTests.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Supabase.Functions;
 using static Supabase.Functions.Client;
@@ -16,10 +19,8 @@ namespace FunctionsTests
         [TestInitialize]
         public void Initialize()
         {
-            var endpoint = Environment.GetEnvironmentVariable("FUNCTION_ENDPOINT");
-
-            _token = Environment.GetEnvironmentVariable("TOKEN");
-            _client = new Client(endpoint);
+            _token = GenerateToken("37c304f8-51aa-419a-a1af-06154e63707a");
+            _client = new Client("http://localhost:9000");
         }
 
         [TestMethod("Invokes a function.")]
@@ -62,6 +63,20 @@ namespace FunctionsTests
             var bytes = await result3.ReadAsByteArrayAsync();
 
             Assert.IsInstanceOfType(bytes, typeof(byte[]));
+        }
+
+        private static string GenerateToken(string secret)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(securityToken);
         }
     }
 }
