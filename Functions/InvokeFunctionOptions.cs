fb3fe6a8221bb42bcf4c813c01695df5c857809c
Diego Santo <diegofesanto@outlook.com>
2025-04-25 23:39:07 -0300

    chore: documentation method to avoid github warnings

--- Functions/InvokeFunctionOptions.cs
+++ Functions/InvokeFunctionOptions.cs
@@ -50,38 +50,83 @@ namespace Supabase.Functions
         {
             private string _region;
 
+            /// <summary>
+            /// Empty region
+            /// </summary>
             public static FunctionRegion Any { get; } = new FunctionRegion("any");
 
+            /// <summary>
+            /// Represents the region "ap-northeast-1" for function requests.
+            /// </summary>
             public static FunctionRegion ApNortheast1 { get; } =
                 new FunctionRegion("ap-northeast-1");
 
+            /// <summary>
+            /// Represents the "ap-northeast-2" region for function invocation.
+            /// </summary>
             public static FunctionRegion ApNortheast2 { get; } =
                 new FunctionRegion("ap-northeast-2");
 
+            /// <summary>
+            /// Represents the "ap-south-1" region used for requests.
+            /// </summary>
             public static FunctionRegion ApSouth1 { get; } = new FunctionRegion("ap-south-1");
 
+            /// <summary>
+            /// Represents the region "ap-southeast-1" for function invocation.
+            /// </summary>
             public static FunctionRegion ApSoutheast1 { get; } =
                 new FunctionRegion("ap-southeast-1");
 
+            /// <summary>
+            /// Represents the "ap-southeast-2" region for requests.
+            /// </summary>
             public static FunctionRegion ApSoutheast2 { get; } =
                 new FunctionRegion("ap-southeast-2");
 
+            /// <summary>
+            /// Represents the Canada (Central) region for requests.
+            /// </summary>
             public static FunctionRegion CaCentral1 { get; } = new FunctionRegion("ca-central-1");
 
+            /// <summary>
+            /// Represents the "eu-central-1" region for function invocation.
+            /// </summary>
             public static FunctionRegion EuCentral1 { get; } = new FunctionRegion("eu-central-1");
 
+            /// <summary>
+            /// Represents the "eu-west-1" function region for requests.
+            /// </summary>
             public static FunctionRegion EuWest1 { get; } = new FunctionRegion("eu-west-1");
 
+            /// <summary>
+            /// Represents the "eu-west-2" region for function invocation requests.
+            /// </summary>
             public static FunctionRegion EuWest2 { get; } = new FunctionRegion("eu-west-2");
 
+            /// <summary>
+            /// Represents the AWS region 'eu-west-3'.
+            /// </summary>
             public static FunctionRegion EuWest3 { get; } = new FunctionRegion("eu-west-3");
 
+            /// <summary>
+            /// Represents the South America (São Paulo) region for requests.
+            /// </summary>
             public static FunctionRegion SaEast1 { get; } = new FunctionRegion("sa-east-1");
 
+            /// <summary>
+            /// Represents the "us-east-1" region for function requests.
+            /// </summary>
             public static FunctionRegion UsEast1 { get; } = new FunctionRegion("us-east-1");
 
+            /// <summary>
+            /// Represents the us-west-1 region for function requests.
+            /// </summary>
             public static FunctionRegion UsWest1 { get; } = new FunctionRegion("us-west-1");
 
+            /// <summary>
+            /// Represents the "us-west-2" region for requests.
+            /// </summary>
             public static FunctionRegion UsWest2 { get; } = new FunctionRegion("us-west-2");
 
             /// <summary>
@@ -128,8 +173,14 @@ namespace Supabase.Functions
             public static bool operator !=(FunctionRegion left, FunctionRegion right) =>
                 !Equals(left, right);
 
+            /// <summary>
+            /// Overloads the explicit cast operator to convert a FunctionRegion object to a string.
+            /// </summary>
             public static explicit operator string(FunctionRegion region) => region.ToString();
 
+            /// <summary>
+            /// Overloads the explicit cast operator to convert a string to a FunctionRegion object.
+            /// </summary>
             public static explicit operator FunctionRegion(string region) =>
                 new FunctionRegion(region);
 
